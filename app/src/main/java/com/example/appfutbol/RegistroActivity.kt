4c852432cdb0d3aa19194832358779e22a473422
Joaquin Liniers <liniersjoaquin@gmail.com>
2025-09-04 22:12:34 -0300

    Vista Registro - maquetado inicial

---
+++ app/src/main/java/com/example/appfutbol/RegistroActivity.kt
@@ -0,0 +1,28 @@
+package com.example.appfutbol
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistroActivity : AppCompatActivity() {
+
+    lateinit var btnRegistro: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnRegistro=findViewById<Button>(R.id.btnRegistro)
+
+            //TODO: INTENT HACIA LoginActivity
+    }
+}
