668378eb57a541ea56b44dd0719288d298542b11
Joaquin Liniers <liniersjoaquin@gmail.com>
2025-09-05 17:08:42 -0300

    Maquetado inicial de la pantalla main

--- app/src/main/java/com/example/appfutbol/MainActivity.kt
+++ app/src/main/java/com/example/appfutbol/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.appfutbol
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRegistrarme: Button
+    lateinit var btnIniciar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnRegistrarme=findViewById<Button>(R.id.btnRegistro)
+        btnIniciar=findViewById<Button>(R.id.btnIniciar)
+
+        btnRegistrarme.setOnClickListener {
+
+            //TODO: INTENT A REGISTRO
+        }
+
+        btnRegistrarme.setOnClickListener {
+
+            //TODO: INTENT A LOGIN
         }
     }
 }
