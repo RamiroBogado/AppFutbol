70e9914c5b63cd665ccd71f36af9f5f1a8e09aca
Joaquin Liniers <liniersjoaquin@gmail.com>
2025-09-19 18:00:38 -0300

    Correcciones generales para corregir errores

--- app/src/main/java/UsuarioDao.kt
+++ app/src/main/java/UsuarioDao.kt
@@ -7,13 +7,13 @@ interface UsuarioDao {
 
     // Insertar un usuario
     @Insert
-    suspend fun insert(usuario: Usuario)
+    fun insert(usuario: Usuario)
 
     // Obtener un usuario por nombre de usuario (para comprobar existencia)
     @Query("SELECT * FROM usuarios WHERE usuario = :nombreUsuario LIMIT 1")
-    suspend fun getByUsuario(nombreUsuario: String): Usuario?
+    fun getByUsuario(nombreUsuario: String): Usuario?
 
     // Verificar usuario y contraseña
     @Query("SELECT * FROM usuarios WHERE usuario = :nombreUsuario AND pass = :contrasena LIMIT 1")
-    suspend fun login(nombreUsuario: String, contrasena: String): Usuario?
+    fun login(nombreUsuario: String, contrasena: String): Usuario?
 }
